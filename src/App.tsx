fcc54d8df9cb7617c1e7d4ccd5d1f145f75b8914
Farid Amimri <farid.amimri@gmail.com>
2023-10-09 11:20:49 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+const Header = () => {
+  return (
+    <div className='flex m-2 w-screen'>
+      <div className='flex-1'>logo</div>
+      <div className='flex-2'>navigation</div>
+      <div className='flex-1'>book now</div>
+    </div>
+  )
+}
+
+export default Header
