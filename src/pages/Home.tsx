c79ce1947e8dfc66a281fcb8cc621682c445b73d
Farid Amimri <farid.amimri@gmail.com>
2023-10-12 15:20:21 +0200

    Add CallToAction section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@
 
 import Hero from '../components/Hero'
 import Booking from '../components/Booking'
+import CallToAction from '../components/CallToAction'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Booking />
+      <CallToAction />
     </div>
   )
 }
