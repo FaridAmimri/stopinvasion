d374f0918114498561cbada5c289d166d8bc9986
Farid Amimri <farid.amimri@gmail.com>
2023-10-12 11:02:37 +0200

    Add Booking section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 /** @format */
 
 import Hero from '../components/Hero'
+import Booking from '../components/Booking'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Booking />
     </div>
   )
 }
