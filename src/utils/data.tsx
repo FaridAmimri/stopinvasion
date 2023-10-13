389dfa45a88b50c26802081d36c2521284fb8556
Farid Amimri <farid.amimri@gmail.com>
2023-10-13 11:24:46 +0200

    Create data file

---
+++ src/utils/data.tsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import { MenuType } from '../types/types'
+
+export const menu: MenuType = [
+  { id: 1, name: 'Home', link: '/' },
+  { id: 2, name: 'Services', link: '/services' },
+  { id: 3, name: 'About us', link: '/about' },
+  { id: 4, name: 'Contact', link: '/contact' }
+]
